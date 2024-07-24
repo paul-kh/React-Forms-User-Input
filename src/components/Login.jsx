46710937b86c0f3a8a436aacd41b2a1af1809b54
Paul Chheang <phalcheat@gmail.com>
2024-07-24 11:55:16 -0400

    Basic way of getting user input

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 export default function Login() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
   function handleSubmit(event) {
     event.preventDefault();
     console.log("submitted");
+    console.log("User email: ", enteredEmail);
+    console.log("User password: ", enteredPassword);
   }
 
+  function handleEmailChange(event) {
+    setEnteredEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setEnteredPassword(event.target.value);
+  }
   return (
     /* The elegant way to prevent the default behavior of form submission:
          - specify the onSubmit = {handleSubmit} in the form element,
@@ -15,12 +29,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={handleEmailChange}
+            value={enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={handlePasswordChange}
+            value={enteredPassword}
+          />
         </div>
       </div>
 
